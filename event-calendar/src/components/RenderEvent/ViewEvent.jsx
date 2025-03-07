b48e8af693c5a55424a5ffb3356180e79f9473c6
VikajaK1 <nikolay.kodzheykov.a67@learn.telerikacademy.com>
2025-03-07 13:47:47 +0200

    Fix typo in event service import and restructure services; remove obsolete files

--- event-calendar/src/components/RenderEvent/ViewEvent.jsx
+++ event-calendar/src/components/RenderEvent/ViewEvent.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getEventById } from "../../services/eventServise";
+import { getEventById } from "../../services/eventService";
 
 function RenderEvent() {
   const { id } = useParams();
