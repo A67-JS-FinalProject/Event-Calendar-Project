255a894506006395793c869aba5c7ce617fe2c70
Martin Mesechkov <mesechkov.martin@gmail.com>
2025-03-07 14:25:15 +0200

    Add Firebase configuration and update dependencies

---
+++ event-calendar/src/config/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD_K-NQ2zbY-Xk3jb1IHGRPExx45Y6eRT8",
+  authDomain: "eventcalendar-2a6d3.firebaseapp.com",
+  projectId: "eventcalendar-2a6d3",
+  storageBucket: "eventcalendar-2a6d3.firebasestorage.app",
+  messagingSenderId: "209712143196",
+  appId: "1:209712143196:web:3feb47b7f8de4391e0ff99",
+  measurementId: "G-NZT534HJ1H",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const authObject = getAuth(app);
