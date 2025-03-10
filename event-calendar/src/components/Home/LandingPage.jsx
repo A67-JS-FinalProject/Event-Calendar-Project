906ed7ba772e35512a4700404199cab0b8e21e99
Martin Mesechkov <mesechkov.martin@gmail.com>
2025-03-10 14:24:49 +0200

    Fix LandingPage import typo; wrap Home component with Authenticated HOC; enhance registration flow with token handling

--- event-calendar/src/components/Home/LandingPage.jsx
+++ event-calendar/src/components/Home/LandingPage.jsx
@@ -3,19 +3,9 @@ import { Link } from "react-router-dom";
 function LandingPage() {
   return (
     <div className="home">
-      <h2>Home</h2>
+      <h2>Landing Page</h2>
       <nav>
         <ul>
-          <li>
-            <Link to="/create-an-event">Create An Event</Link>
-          </li>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/event/67c9de56ff7fc07b94f36e96">View Event</Link>{" "}
-            {/*change the id*/}
-          </li>
           <li>
             <Link to="/register">Register</Link>
           </li>
