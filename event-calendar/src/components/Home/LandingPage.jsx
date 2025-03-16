eaa4d5e3ecbaca684e98410cd2e839f30c5e8832
Martin Mesechkov <mesechkov.martin@gmail.com>
2025-03-16 22:02:02 +0200

    Add NavBarPublic component and enhance LandingPage layout

--- event-calendar/src/components/Home/LandingPage.jsx
+++ event-calendar/src/components/Home/LandingPage.jsx
@@ -1,19 +1,19 @@
-import { Link } from "react-router-dom";
+import NavBarPublic from "../NavBarPublic/NavBarPublic";
 
 function LandingPage() {
   return (
-    <div className="home">
-      <h2>Landing Page</h2>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="home text-center">
+      <NavBarPublic />
+      <div className="flex flex-col items-center text-center mt-16 px-5">
+        <h1 className="text-4xl font-bold">
+          Stay Ahead with Our <br />
+          <span className="text-orange-600">Event Calendar</span>
+        </h1>
+        <button className="btn btn-error text-white mt-6 px-20 py-6 text-lg rounded-full">
+          START IMPROVING YOUR ORGANIZATION TODAY
+        </button>
+        <div className="mt-12"></div>
+      </div>
     </div>
   );
 }
---
+++ event-calendar/src/components/NavBarPublic/NavBarPublic.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+export default function NavBarPublic() {
+    return (
+        <nav className="navbar flex justify-center items-center bg-gray-500 p-6">
+            <Link to="/login" className="btn btn-error text-white px-6 py-3 text-lg rounded-full mr-6">
+                About Us
+            </Link>
+            <label className="input mr-6">
+                <svg className="h-[1em] opacity-50" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
+                <input type="search" required placeholder="Search Events" />
+            </label>
+            <div className="flex space-x-10">
+                <Link to="/login" className="btn btn-error text-white px-6 py-3 text-lg rounded-full">
+                    Login
+                </Link>
+                <Link to="/register" className="btn btn-error text-white px-6 py-3 text-lg rounded-full">
+                    Register
+                </Link>
+            </div>
+        </nav>
+    );
+}
